5ee0f89bf7cec80c8353d0dee423bcafb30d5f7b
rf-santos <ricardofilipecdsantos@gmail.com>
2021-12-13 15:49:43 +0000

    Pull request ml-dev -> master

--- test/cpat/cpat.r
+++
@@ -1,4 +0,0 @@
-load("/usr/bin/app/test/cpat/Fly_logitModel.RData")
-test <- read.table(file="/usr/bin/app/test/cpat/cpat.ORF_info.tsv",sep="\t",header=T)
-test$Coding_prob <- predict(mylogit,newdata=test,type="response")
-write.table(test, file="/usr/bin/app/test/cpat/cpat.ORF_prob.tsv", quote=F, sep="\t",row.names=FALSE, col.names=TRUE)
